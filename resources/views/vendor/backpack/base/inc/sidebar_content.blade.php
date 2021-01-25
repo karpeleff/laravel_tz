80d0bcd675efa01a0559fdeb3f2505934f97c90f
User <alekseykarpelev@gmail.com>
2021-01-25 11:11:22 +1000

    out

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,5 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tag') }}'><i class='nav-icon la la-question'></i> Tags</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('book') }}'><i class='nav-icon la la-question'></i> Books</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('autor') }}'><i class='nav-icon la la-question'></i> Autors</a></li>
